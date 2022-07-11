4fc64d6aee948876b2ad9754473565a8ac52f348
Zaid Sheikh <zsheikh@cs.cmu.edu>
2022-07-11 10:39:59 +0530

    multi-file uploads

--- ocr_frontend/src/App.js
+++ ocr_frontend/src/App.js
@@ -14,21 +14,29 @@ function App() {
   const [text, setText] = useState()
 
   function handleChange(event) {
-    setFile(event.target.files[0])
+    console.log(event.target.files)
+    console.log(Array.from(event.target.files))
+    //setFile(event.target.files[0])
+    setFile(Array.from(event.target.files))
   }
 
   function handleSubmit(event) {
     event.preventDefault()
-    const url = 'http://localhost:8088/annotator/ocr-post-correction/';
+    //const url = 'http://localhost:8088/annotator/ocr-post-correction/';
+    const url = 'http://rabat.sp.cs.cmu.edu:8088/annotator/ocr-post-correction/';
     //const url = '/annotator/ocr-post-correction/';
     const formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', file.name);
+    file.forEach(f=>{
+        formData.append('file', f);
+    });
+    //formData.append('file', file);
+    //formData.append('fileName', file.name);
     formData.append('params', '{"debug": 1}')
     const config = {
       headers: {
         'content-type': 'multipart/form-data',
-        'Authorization': '8470ede027588b80c5b82ab5c9e78b8daea68635'
+        //'Authorization': '8470ede027588b80c5b82ab5c9e78b8daea68635'
+        'Authorization': '5e72d818c2f4250687f090bb7ec5466184982edc'
       },
     };
     axios.post(url, formData, config).then((response) => {
@@ -42,7 +50,7 @@ function App() {
     <div className="App">
       <form onSubmit={handleSubmit}>
         <h1>Upload image or pdf</h1>
-        <input type="file" onChange={handleChange}/>
+        <input type="file" multiple onChange={handleChange}/>
         <button type="submit">Upload</button>
         <div>
           {JSON.stringify(text, null, 2)}
