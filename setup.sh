a6c76382e9f706f270c8f33c695fc90e2a22f0aa
Zaid Sheikh <zsheikh@cs.cmu.edu>
2022-09-21 10:09:22 +0530

    update Dockerfile and setup scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cd /
+git clone https://github.com/shrutirij/ocr-post-correction
+cd ocr-post-correction/
+conda create --yes --name ocr-post-correction python=3.9.5
+conda activate ocr-post-correction
+pip install -r postcorr_requirements.txt
+pip install -r ocr_requirements.txt
+cp /cmulab/tests/cmulab_ocr_t*.sh .
+cp /cmulab/tests/echo_cmulab_ocr_t*.sh .
 
 conda create --yes --name cmulab python=3.7.10
 conda activate cmulab
@@ -7,6 +16,7 @@ python setup.py develop
 pip install git+https://github.com/zaidsheikh/cmulab_allosaurus
 pip install git+https://github.com/zaidsheikh/cmulab_diarization
 pip install git+https://github.com/zaidsheikh/cmulab_stanza
+ln -s /dev/shm/.config/.env.yml .
 
 python manage.py makemigrations annotator
 python manage.py migrate
